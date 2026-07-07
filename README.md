347a2cedc9a7e87697fd3fd26e73ebcf8fcd5a77
Himanshu Sah <himanshusah@himanshus-air.mynetworksettings.com>
2026-07-06 18:08:50 -0700

    NewUser class created

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Habit Tracker -- CLI
 
-A python CLI(command line interface) application to track habit.
+A python [CLI](https://github.com/resources/articles/what-is-a-cli) (command line interface) application to track habit.
 
 ## Introduction
 This application adds, removes, marks as completed, and delete daily habits as requested by the user. It shows what percentage of the days those habits where followed in the current month as well as in the current year.
---
+++ src/main.py
@@ -0,0 +1,82 @@
+# This application adds, removes, marks as completed, and delete daily habits as requested by the user. 
+# It shows what percentage of the days those habits where followed in the current month as well as in the current year.
+
+from datetime import datetime
+import os
+
+def clear():
+    _ = os.system('cls' if os.name == 'nt' else 'clear')
+
+# Call it whenever you need
+clear()
+
+# Creating a class for the user creation.
+class NewUser:
+    def __init__(self, username, password, name, email):
+        self.username = username
+        self.password = password
+        self.name = name
+        self.email = email
+
+    def check_input(self):
+        if(len(self.username) < 6):
+            print("Username is not valid!")
+            return False
+        if(len(self.password) < 6):
+            print("Password is invalid!")
+            return False
+        if(len(self.name) < 2):
+            print("Name is invalid!")
+            return False
+        if(len(self.email) < 4 or '@' not in self.email or '.com' not in self.email):
+            print("Email is invalid!")
+            return False
+        return True
+
+class Task:
+    def __init__(self, name, time):
+        self.name = name
+        self.time = time
+
+def create_account():
+    username = input("Please enter your username! (At least 6 digits) ")
+    password = input("Please enter your password! (At least 6 digits) ")
+    name = input("Please enter your name! (ex. John Doe) ")
+    email = input("Please enter your email! (ex. johndoe@gmail.com) ")
+
+    newUser = NewUser(username, password, name, email)
+    if(newUser.check_input()):
+        print("Your account has been created. Please sign in!")
+        store_new_account(newUser)
+        input()
+    else:
+        print("Please try again with the updated information!")
+        input()
+
+def store_new_account(newUser):
+    print(newUser)
+    print("New account has been stored!")
+    pass
+
+program_running = True
+        
+while(program_running == True):
+    print(f"Welcome to the habit tracker!")
+    print(f"Please select one of the options below!")
+    print(f"1. Login.")
+    print(f"2. Create a new user.")
+    print(f"3. Exit.")
+    option1 = int(input("Enter your choice(1-3) "))
+    if(option1 == 1):
+        pass
+    elif(option1 == 2):
+        create_account()
+        pass
+    elif(option1 == 3):
+        program_running = False
+        pass
+    else:
+        print(f"Invalid option. Please try again!")
+        pass
+
+
